6fdbb2805d31950c15e79c3832d7d8bb363dd683
Etiene Cristina <etiene.scristina@gmail.com>
2021-06-15 22:27:03 -0300

    Adição do evento de finalizar a activity ao clicar no botão "Próximo"

--- app/src/main/java/br/com/etiene/cadastrocliente/CadastroClienteActivity.java
+++ app/src/main/java/br/com/etiene/cadastrocliente/CadastroClienteActivity.java
@@ -2,14 +2,29 @@ package br.com.etiene.cadastrocliente;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CadastroClienteActivity extends AppCompatActivity {
+
+    Button bottonProximo;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cadastro_cliente_activity);
+
+        bottonProximo = findViewById(R.id.bottonProximo);{
+        bottonProximo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        }
     }
 }
