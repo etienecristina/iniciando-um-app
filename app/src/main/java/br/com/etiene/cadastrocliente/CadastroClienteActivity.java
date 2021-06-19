c3bbac3ec30aad412a893b4d3ee9e554f28df1ff
Etiene Cristina <etiene.scristina@gmail.com>
2021-06-18 22:05:03 -0300

    Alteração no arquivo XML (campo Nome) para receber textos, sem sugestões para preenchimento

--- app/src/main/java/br/com/etiene/cadastrocliente/CadastroClienteActivity.java
+++ app/src/main/java/br/com/etiene/cadastrocliente/CadastroClienteActivity.java
@@ -2,9 +2,11 @@ package br.com.etiene.cadastrocliente;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,19 +14,22 @@ import androidx.appcompat.app.AppCompatActivity;
 public class CadastroClienteActivity extends AppCompatActivity {
 
     Button bottonProximo;
+    EditText nome;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cadastro_cliente_activity);
 
-        bottonProximo = findViewById(R.id.botao);{
+        bottonProximo = findViewById(R.id.botao);
         bottonProximo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
-        }
+
+
+
     }
 }
